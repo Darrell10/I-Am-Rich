f87eb3d4ed5edd72c995b7822ec5f9a4166886c4
Darrell10 <frederick.port@me.com>
2020-06-04 13:07:55 +0200

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Frederick Port on 04/06/2020.
+//  Copyright © 2020 stud-iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
